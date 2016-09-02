d31ae37c4ab739f5b04876dfdf6382d50f0ea167
Tasi <tasi@Ninos-MacBook-Pro.local>
2016-09-02 20:40:18 +0100

    cjsm,z

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -17,7 +17,16 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         tableview.dataSource = self
         tableview.delegate = self
     }
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Hello There"
+        return cell
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
